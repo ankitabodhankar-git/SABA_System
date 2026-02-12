7c9a3a18eb7c4f165d1b33725615f6ddddee0f99
Ankitab <ankitabodhankar@gmail.com>
2026-02-12 11:52:24 +0530

    Design login page UI

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "SABA System is Running Successfully"
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
